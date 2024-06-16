c23be6e07eb458248f638fc3267bd06c68fd2c62
Bruce <aaaeeettt124@126.com>
2024-06-16 22:42:28 +0800

    new

--- front/main.go
+++ front/main.go
@@ -7,20 +7,25 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/**/*")
-	//r.LoadHTMLFiles("templates/posts/index.html", "templates/users/index.html")
-	r.GET("/posts/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "posts/index.html", gin.H{
-			"title": "posts/index",
+	router := gin.Default()
+	// 首先加载templates目录下面的所有模版文件，模版文件扩展名随意
+	router.LoadHTMLGlob("templates/*")
+
+	//  /assets/images/1.jpg 这个url文件，存储在/public/images/1.jpg
+	router.Static("/assets", "public")
+
+	// 为单个静态资源文件，绑定url
+	// 这里的意思就是将/favicon.ico这个url，绑定到./resources/favicon.ico这个文件
+	//router.StaticFile("/favicon.ico", "./resources/favicon.ico")
+
+	router.GET("/", func(c *gin.Context) {
+		// 通过HTML函数返回html代码
+		// 第二个参数是模版文件名字
+		// 第三个参数是map类型，代表模版参数
+		// gin.H 是map[string]interface{}类型的别名
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Yujun Chen's Personal Website",
 		})
 	})
-
-	r.GET("users/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "users/index.html", gin.H{
-			"title": "users/index",
-		})
-	})
-
-	r.Run(":8080")
+	router.Run(":8080")
 }
--- hello_gin/middleware.go
+++ hello_gin/middleware.go
@@ -27,3 +27,26 @@ func StatCost() gin.HandlerFunc {
 		log.Println("------------", cost)
 	}
 }
+
+// 自定义个日志中间件
+func Loggger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		// 可以通过上下文对象，设置一些依附在上下文对象里面的键/值数据
+		c.Set("example", "12345")
+
+		// 在这里处理请求到达控制器函数之前的逻辑
+
+		// 调用下一个中间件，或者控制器处理函数，具体得看注册了多少个中间件。
+		c.Next()
+
+		// 在这里可以处理请求返回给用户之前的逻辑
+		latency := time.Since(t)
+		log.Print(latency)
+
+		// 例如，查询请求状态吗
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
