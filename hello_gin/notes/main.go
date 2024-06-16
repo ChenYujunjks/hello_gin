848b5d8e5f6ee65f55559de3d4d953edbe6976c5
Bruce <aaaeeettt124@126.com>
2024-06-16 13:23:00 +0800

    notes

---
+++ hello_gin/notes/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	routes "hello_gin/routers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AdditionRequest struct {
+	Number1 float64 `json:"number1" binding:"required"`
+	Number2 float64 `json:"number2" binding:"required"`
+}
+
+func getStudentData() gin.H {
+	return gin.H{
+		"name": "王石头",
+	}
+}
+func studentHandler(c *gin.Context) {
+	data := getStudentData()
+	c.JSON(http.StatusOK, data)
+}
+func schoolHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"school": "idk", "students": getStudentData()})
+}
+func main() {
+	gin.SetMode(gin.DebugMode) // 设置Gin的运行模式为DebugMode
+
+	r := gin.Default()
+	r.LoadHTMLGlob("../templates/*")
+
+	routes.RegisterRoutes(r)
+	r.GET("/student", studentHandler)
+	r.GET("/school", schoolHandler)
+
+	// JSON 用法
+	r.GET("/ping3", func(c *gin.Context) {
+		type Response struct {
+			Message string `json:"message"`
+		}
+		c.JSON(http.StatusOK, Response{
+			Message: "pong",
+		})
+	})
+	r.Run(":8080")
+}
